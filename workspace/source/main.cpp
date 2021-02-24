31f430f9430a5ed5ef81dd86afa03c6df803ff4e
George M <georgem@stu.pku.edu.cn>
2021-02-24 17:42:16 +0800

    a2

--- workspace/source/main.cpp
+++ workspace/source/main.cpp
@@ -6,12 +6,13 @@ int main()
     std::cin >> n;
     n *= 2;
     std::cout << "Hello world! " << std::endl;
-    int ans;
-    for (int i = 0; i < n; i++)
+    int ans = 1;
+    for (int i = 1; i < n; i++)
     {
         ans *= i;
     }
     std::cout << ans << std::endl;
+    std::cout << "another push" << std::endl;
     system("pause");
     return 0;
 }
